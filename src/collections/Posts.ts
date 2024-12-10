f2371a927daedf5c10c2b0192f3e2cae562947bf
Valentin Ilas <valentin.ilas@gmail.com>
2024-12-10 14:01:10 +0100

    Added more field types

--- src/collections/Posts.ts
+++ src/collections/Posts.ts
@@ -1,15 +1,37 @@
 import type { CollectionConfig } from 'payload'
 
 export const Posts: CollectionConfig = {
-    slug: 'posts',
-    fields: [
-      {
-        name: 'title',
-        type: 'text',
-      },
-      {
-        name: 'Summary',
-        type: 'text',
-      }
-    ]
-  }
+  slug: 'posts',
+  access: {
+    // Allow anyone (even unauthenticated users) to read posts
+    read: () => true,
+  },
+  fields: [
+    {
+      name: 'Slug',
+      type: 'text',
+    },
+    {
+      name: 'Author',
+      type: 'text',
+    },
+    {
+      name: 'Title',
+      type: 'text',
+    },
+
+    {
+      name: 'Summary',
+      type: 'text',
+    },
+    {
+      name: 'Body Text',
+      type: 'textarea',
+    },
+    {
+      name: 'Image', // The name of the field for image uploads
+      type: 'upload', // The field type for file uploads
+      relationTo: 'media', // If you want to store the image in a separate collection called 'media'
+    },
+  ]
+}
