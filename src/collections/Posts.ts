31620b85316d800c769ffca72c447ff0b7fb5602
Valentin Ilas <valentin.ilas@gmail.com>
2024-12-11 03:09:25 +0100

    Added automatic slug for posts

--- src/collections/Posts.ts
+++ src/collections/Posts.ts
@@ -1,4 +1,6 @@
 import type { CollectionConfig } from 'payload'
+import slugify from 'slugify'
+
 
 export const Posts: CollectionConfig = {
   slug: 'posts',
@@ -6,10 +8,30 @@ export const Posts: CollectionConfig = {
     // Allow anyone (even unauthenticated users) to read posts
     read: () => true,
   },
+  hooks: {
+    beforeValidate: [
+      ({ data }) => {
+        console.log(typeof data?.Title);
+        // Generate slug from title if not already provided
+        if (data && data.Title && (!data.Slug || data.slug.trim() === '')) {
+          const timestamp = new Date().getTime(); // Current timestamp in milliseconds
+          const sluggedTitle = slugify(data.Title, { lower: true, strict: true });
+          data.Slug = `${timestamp}-${sluggedTitle}`;
+        }
+        console.log(data);
+        return data;
+      }
+    ]
+  },
   fields: [
     {
       name: 'Slug',
       type: 'text',
+      unique: true, // Ensure slug uniqueness
+      admin:{
+        readOnly: true,
+        description: 'Auto generated on Save'
+      }
     },
     {
       name: 'Author',
