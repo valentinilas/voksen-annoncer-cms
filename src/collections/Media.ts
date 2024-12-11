120a1a6bedb836fff2f30cce57538a4a690a7ed8
Valentin Ilas <valentin.ilas@gmail.com>
2024-12-11 01:59:57 +0100

    Added UUID for images

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,4 +1,5 @@
 import type { CollectionConfig } from 'payload'
+import { v4 as uuidv4 } from 'uuid';  // Import the UUID function
 
 export const Media: CollectionConfig = {
   slug: 'media',
@@ -13,5 +14,19 @@ export const Media: CollectionConfig = {
       required: true,
     },
   ],
+  hooks: {
+    beforeOperation: [
+      async (req) => {
+        if (req.operation === 'create') {
+          if (req.req.file) {
+            // Generate a new UUID for the file name
+            const fileExtension = req.req.file.name.split('.').pop(); // Extract file extension
+            const newFileName = `${uuidv4()}.${fileExtension}`; // Generate UUID and append the file extension
+            req.req.file.name = newFileName; // Assign the new name
+          }
+        }
+      },
+    ],
+  },
   upload: true,
 }
